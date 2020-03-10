bd452ce2277d27d16b840dd6a7c903d78aa31bd8
Alberino Junior <alberino.rijo@outlook.com>
2020-03-10 17:50:41 -0300

    funções js

--- style.js
+++ style.js
@@ -1,3 +1,16 @@
-function mostraAlerta() {
-    alert('Botão clicado')
+var inputElement = document.querySelector('input[name=nome]');
+var btnElement = document.querySelector('button.botao');
+
+//var inputElement = document.querySelector('body div#app input')//pode referenciar atributos também o query sempre retorna um elemento.
+btnElement.onclick = function(){
+    var text = inputElement.value;
+    alert('text');
 }
+console.log(btnElement);
+/*var inputElement = document.getElementById('nome')
+
+console.log(inputElement);
+
+/*function mostraAlerta() {
+    alert('Botão clicado')
+}*/
